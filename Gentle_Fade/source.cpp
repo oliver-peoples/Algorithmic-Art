7e7c9f668959a34a448cdad1f2f04f5772994298
Finn <s3725341@student.rmit.edu.au>
2021-03-20 17:08:28 +1100

    Blend Video

--- Gentle_Fade/source.cpp
+++ Gentle_Fade/source.cpp
@@ -5,9 +5,11 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
 
 int main()
 {
+    cv::VideoWriter blending_clip("../blend.mp4", cv::VideoWriter::fourcc('m','p','4','v'), 30, { 512,512 }, true);
     cv::Mat pepper_1 = cv::imread("../pepper.bmp");
     cv::Mat pepper_2 = cv::imread("../pepper2.bmp");
 
@@ -15,15 +17,18 @@ int main()
 
     cv::Mat blended;
     
-    for (double alpha = 1.0; alpha >= 0.0; alpha -= 0.01)
+    for (double alpha = 1.0; alpha >= 0.0; alpha -= 0.001)
     {
         beta = 1 - alpha;
 
         cv::addWeighted(pepper_1, alpha, pepper_2, beta, 0.0, blended);
 
+        blending_clip.write(blended);
         cv::imshow("Blended", blended);
         cv::waitKey(10);
     }
+
+    blending_clip.release();
     
     return 0;   
 }
--- Perlin_Lines_1/source.cpp
+++ Perlin_Lines_1/source.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <hmath.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
 
 int main()
 {
-    return 0;   
+    cv::Vec2i aspect_ratio = { 16,9 };
+
+    int grid_scale = 30;
+    int img_scale = 400;
+
+    cv::Size image_resolution = { aspect_ratio[0] * img_scale,aspect_ratio[1] * img_scale };
+    cv::Size grid_resolution = { aspect_ratio[0] * grid_scale,aspect_ratio[1] * grid_scale };
+
+    hmath::
 }
